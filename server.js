aa3d97c40e36625ce14ef903bccf48874c195935
JesusKid <frawllings@gmail.com>
2022-11-24 09:59:05 +0100

    updated moralis start

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const AppId = process.env.APP_ID
 const ServerUrl = process.env.SERVER_URL;
 const MasterKey = process.env.MASTER_KEY;
 
-const whiteListedOrignsLive = 'https://2spice.link/'
+const whiteListedOrignsLive = 'https://blog.2spice.link/'
 const whiteListedOrignsTest = 'http://localhost:3000/'
 
 // await Moralis.start({ serverUrl: 'https://2nlnyiqavans.usemoralis.com:2053/server', appId: '2veCjTTSOVtcYuw3kCohS7SVFjZPBc8j0nQyFa00', masterKey: 'w4pUrmNaq7RxTH39TilElpboKQr7weZGLFxiGixB' })
