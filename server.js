adb8fdde968df9d2ba72dd8ef2227c123d81064e
JesusKid <frawllings@gmail.com>
2022-11-24 10:27:32 +0100

    updated moralis start

--- server.js
+++ server.js
@@ -46,7 +46,7 @@ const likePostMain = async (mpId, email) => {
 
 
 app.post('/like', (req, res) => {
-    const host = req.get('host');
+    const host = req.get('origin');
     console.log(host)
     if (host == whiteListedOrignsLive || host == whiteListedOrignsTest) {
         likePostMain(data.mpId, data.email)
